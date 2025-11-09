b74124eff69e7dcdaf31a2abfaff66d750d0093b
WhyFenceCode <whyfencecode@gmail.com>
2025-11-09 12:25:32 -0800

    re-wrote a lot of ripple.ts

--- src/scripts/ripple.ts
+++ src/scripts/ripple.ts
@@ -6,68 +6,77 @@ import displayShader from '../shaders/display.glsl?raw';
 
 
 export async function initRipple() {
+  //Get Current Canvases
   const renderCanvas = document.getElementById('ripple-canvas') as HTMLCanvasElement;
   const hiddenDiv = document.getElementById('offscreen-division') as HTMLElement;
   if (!renderCanvas || !hiddenDiv) return;
 
+  //Build a background texture from the html (TODO fix flex it breaks here)
   const divCanvas = await html2canvas(hiddenDiv);
   const texture = new THREE.CanvasTexture(divCanvas);
 
+  //Ensure a clean canvas by replacing the existing one
   let newCanvas = document.createElement('canvas');
   newCanvas.id = 'ripple-canvas';
   newCanvas.className = 'w-dvw h-dvh';
   renderCanvas.parentNode?.replaceChild(newCanvas, renderCanvas);
 
+  //Write a storage canvas for the effect buffer
   let pNewCanvas = document.createElement('canvas');
   pNewCanvas.id = 'pressure-ripple-canvas';
   pNewCanvas.className = 'w-dvw h-dvh';
-  renderCanvas.parentNode?.appendChild(pNewCanvas);
+  newCanvas.parentNode?.appendChild(pNewCanvas);
 
+  //Set up both renders
   const renderer = new THREE.WebGLRenderer({ canvas: newCanvas, antialias: false });
   const pRenderer = new THREE.WebGLRenderer({ canvas: pNewCanvas, antialias: false });
-
   renderer.setSize(window.innerWidth, window.innerHeight);
   pRenderer.setSize(window.innerWidth, window.innerHeight);
 
+  //Build both scenes and respective Orthographic Cameras
   const scene = new THREE.Scene();
   const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1);
-
   const pScene = new THREE.Scene();
   const pCamera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1);
 
   const resolution = new THREE.Vector2(window.innerWidth, window.innerHeight);
 
+  //Build two render targets
   const rt1 = new THREE.WebGLRenderTarget(resolution.x, resolution.y);
   const rt2 = new THREE.WebGLRenderTarget(resolution.x, resolution.y);
-  let prevRT = rt1;
-  let currRT = rt2;
+  let mainRT = rt1;
+  let vRT = rt2;
 
+  //Set Uniforms
   const uniforms = {
     iResolution: { value: new THREE.Vector2(window.innerWidth, window.innerHeight) },
     iTime: { value: 0 },
     iMouse: { value: new THREE.Vector4(0,0,0,0) },
-    iChannel1: { value: prevRT.texture },
+    iChannel1: { value: vRT.texture },
     iChannel0: { value: texture }
-};
+  };
 
+  //Display Material
   const material = new THREE.ShaderMaterial({
     vertexShader,
     fragmentShader:displayShader,
     uniforms
   });
 
+  //Simulation Material
   const pressure = new THREE.ShaderMaterial({
     vertexShader,
     fragmentShader,
     uniforms
   });
 
-  const pressBuffer = new THREE.Mesh(new THREE.PlaneGeometry(2, 2), pressure);
-  pScene.add(pressBuffer);
-
+  //Build both capture meshes
   const mesh = new THREE.Mesh(new THREE.PlaneGeometry(2, 2), material);
   scene.add(mesh);
+  const pMesh = new THREE.Mesh(new THREE.PlaneGeometry(2, 2), pressure);
+  pScene.add(pMesh);
 
+  //Alter uniforms with mouse input
   window.addEventListener('mousemove', (e) => {
     uniforms.iMouse.value.x = e.clientX;
     uniforms.iMouse.value.y = window.innerHeight - e.clientY;
@@ -76,21 +85,21 @@ export async function initRipple() {
 
   let clock = new THREE.Clock();
 
+  //Render and write all buffers
   function animate() {
     uniforms.iTime.value = clock.getElapsedTime();
-    uniforms.iChannel1.value = prevRT.texture;
-
-    renderer.setRenderTarget(currRT);
+    uniforms.iChannel1.value = vRT.texture;
+    renderer.setRenderTarget(mainRT);
     renderer.render(scene, camera);
     renderer.setRenderTarget(null);
 
-    [prevRT, currRT] = [currRT, prevRT];
-
-    pRenderer.setRenderTarget(prevRT);
+    pRenderer.setRenderTarget(vRT);
     pRenderer.render(scene, pCamera);
+    pRenderer.setRenderTarget(null);
 
-    mesh.material.uniforms.iChannel1.value = prevRT.texture;
+    uniforms.iChannel1.value = vRT.texture;
     renderer.render(scene, camera);
+
 
     requestAnimationFrame(animate);
   }
