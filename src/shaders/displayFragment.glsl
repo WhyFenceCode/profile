dcbb40e7ebf00a19bfbbc211b10b37b8c761fbee
WhyFenceCode <whyfencecode@gmail.com>
2025-11-09 21:02:50 -0800

    changed specular level for now

--- src/shaders/displayFragment.glsl
+++ src/shaders/displayFragment.glsl
@@ -12,7 +12,7 @@ void main() {
 
     vec3 normal = normalize(vec3(-data.z * 0.2, 0.5, -data.w * 0.2));
     vec3 lightDir = normalize(vec3(-3.0, 10.0, 3.0));
-    float specular = pow(max(0.0, dot(normal, lightDir)), 60.0) * 1.5;
+    float specular = pow(max(0.0, dot(normal, lightDir)), 60.0) * 0.5;
 
     gl_FragColor = color + vec4(vec3(specular), 0.0);
 }
