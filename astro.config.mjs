7c67944a4ff2151f0ef0eecbf79832c4418bc6ee
WhyFenceCode <whyfencecode@gmail.com>
2025-11-13 14:35:00 -0800

    even more fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
-import glsl from 'vite-plugin-glsl';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss(), glsl()],
+    plugins: [tailwindcss()],
   },
 });
