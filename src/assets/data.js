aeef28e791303a2bee38b6fcb2b182a454dda78b
WhyFenceCode <whyfencecode@gmail.com>
2026-04-22 18:13:33 -0700

    added muti date systems

---
+++ src/assets/data.js
@@ -0,0 +1,12 @@
+export const meStrings = [
+  "High School Senior",
+  "Human, Not AI",
+  "Aspiring Engineer",
+  "Sailing Instructor",
+  "Leader",
+];
+
+export const dateSets = [
+  ["2026-6-9", "UNTIL GRADUATION"],
+  ["2026-9-8", "UNTIL UNIVERSITY"]
+]
